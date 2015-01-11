1b7cabdc0cfdc1718da937a2d284284d34226aa1
malenkix <seleenera@gmx.de>
2015-01-11 15:13:25 +0100

    syntax corrected

--- js/character-example.js
+++ js/character-example.js
@@ -1,15 +1,15 @@
 var character = {
-	name: 'Elon Eibenglanz',
-	experience: 465,
-	attributes: {
-		body: 4
-		strength: 0,
-		toughness: 0,
-		agility: 8,
-		movement: 4,
-		dexterity: 8,
-		mind: 8
-		intellect: 2,
-		aura: 0,
-	}
+    name: 'Elon Eibenglanz',
+    experience: 465,
+    attributes: {
+        body: 4,
+        strength: 0,
+        toughness: 0,
+        agility: 8,
+        movement: 4,
+        dexterity: 8,
+        mind: 8,
+        intellect: 2,
+        aura: 0,
+    }
 };
